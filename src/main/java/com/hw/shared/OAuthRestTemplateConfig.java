1935d724b8a2fcf0e2d649d6a6b921cb58feca9c
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-03-10 22:43:06 -0400

    add MDC & uuid to all requests

--- src/main/java/com/hw/shared/OAuthRestTemplateConfig.java
+++ src/main/java/com/hw/shared/OAuthRestTemplateConfig.java
@@ -2,17 +2,24 @@ package com.hw.shared;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
+
 @Configuration
 public class OAuthRestTemplateConfig {
+
+    @Autowired
+    OutgoingReqInterceptor requestInterceptor;
 
     @Bean
     public RestTemplate getRestTemplate(HttpComponentsClientHttpRequestFactory factory) {
         RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(requestInterceptor));
         restTemplate.setRequestFactory(factory);
         return restTemplate;
     }
