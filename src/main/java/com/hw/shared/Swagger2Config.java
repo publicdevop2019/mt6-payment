042499fd32bdc0518d2995855beec84f53beaca3
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-04-09 20:13:19 -0400

    add new shared file

--- src/main/java/com/hw/shared/Swagger2Config.java
+++ src/main/java/com/hw/shared/Swagger2Config.java
@@ -18,8 +18,8 @@ public class Swagger2Config {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors
-                        .basePackage("com.hw.controller"))
-                .paths(PathSelectors.regex("/.*"))
+                        .basePackage("com.hw"))
+                .paths(PathSelectors.any())
                 .build().apiInfo(apiEndPointsInfo());
     }
     private ApiInfo apiEndPointsInfo() {
