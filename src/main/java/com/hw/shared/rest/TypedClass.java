a78a18332ef075b80b1e906ca60ef2c01679cbdf
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-10-14 09:47:50 -0400

    update shared

--- src/main/java/com/hw/shared/rest/TypedClass.java
+++ src/main/java/com/hw/shared/rest/TypedClass.java
@@ -1,8 +1,10 @@
 package com.hw.shared.rest;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public abstract class TypedClass<T> {
     private Class<T> clazz;
 
